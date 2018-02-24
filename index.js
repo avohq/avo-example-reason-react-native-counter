abccd3ca94b750d68f0ed874a3b46f4e16c0521e
Arnar Þór Sveinsson <arnar.th92@gmail.com>
2018-02-24 23:42:07 +0000

    Analytics working

---
+++ index.js
@@ -0,0 +1,4 @@
+import {AppRegistry} from 'react-native';
+import App from './src/App.bs.js';
+
+AppRegistry.registerComponent('ReasonCounter', () => App);
---
+++ ios/Pods/Target Support Files/Pods-ReasonCounter/Pods-ReasonCounter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReasonCounter : NSObject
+@end
+@implementation PodsDummy_Pods_ReasonCounter
+@end
