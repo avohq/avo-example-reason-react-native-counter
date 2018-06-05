e8a57733b3f946f7974acfc9b55d038f3ad9ae3d
Solvi Logason <logason@gmail.com>
2018-06-05 07:51:42 -0700

    User the __DEV__ flag in setup

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import {AppRegistry} from 'react-native';
 import Analytics from './src/analytics';
 import App from './src/App.bs.js';
 
-// Analytics.setup_({useProductionKey: !__DEV__, validateProperties: __DEV__});
-Analytics.setup_({useProductionKey: true, validateProperties: false});
+Analytics.setup_({useProductionKey: !__DEV__, validateProperties: __DEV__});
 
 AppRegistry.registerComponent('ReasonCounter', () => App);
